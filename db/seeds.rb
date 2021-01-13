e7870772533db354c5bde56763e45837969ccdb9
Zac Willis <zwillis@relevantinc.com>
2021-01-13 16:06:39 -0700

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+50_000.times do |i|
+  Customer.create!(
+    first_name: Faker::Name.first_name,
+     last_name: Faker::Name.last_name,
+      username: "#{Faker::Internet.user_name}#{i}",
+         email: Faker::Internet.user_name + i.to_s +
+                "@#{Faker::Internet.domain_name}")
+  print '.'
+end
+puts
