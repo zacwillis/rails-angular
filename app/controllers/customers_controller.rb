a0c05e65f9d7f7f82317a478fff2240f56b21627
Zac Willis <zwillis@relevantinc.com>
2021-01-14 15:53:38 -0700

    Add customer search functionality.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,23 @@
 class CustomersController < ApplicationController
-
   def index
-    @customers = Customer.all.limit(10)
+    if params[:keywords].present?
+      wr = []
+      keywords = params[:keywords]
+      if keywords =~ /@/
+        wr << "lower(email) Ilike '%#{keywords}%'"
+      else
+        name_array = keywords.split(" ")
+        if name_array.length > 1
+          wr << "lower(first_name) Ilike '#{name_array.first.downcase}%'"
+          wr << "lower(last_name) Ilike '#{name_array.last.downcase}%'"
+        else
+          wr << "(lower(first_name) Ilike '#{name_array.first.downcase}%' or lower(last_name) Ilike '#{name_array.last.downcase}%')"
+        end
+      end
+      where_clause = wr.length > 1 ? wr.join(' and ') : wr.first
+      @customers = Customer.where(where_clause)
+    else
+      @customers = []
+    end
   end
 end
