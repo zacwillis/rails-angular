3b6b7571d11e14c66eb57dfb55ce0649626bf0d5
Zac Willis <zwillis@relevantinc.com>
2021-01-20 11:05:02 -0700

    Display canned results.

--- app/javascript/angular_app/customer_search/customer_search.component.ts
+++ app/javascript/angular_app/customer_search/customer_search.component.ts
@@ -2,6 +2,44 @@ import { Component, NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 
+var RESULTS = [
+  {
+    first_name: "Pat",
+    last_name: "Smith",
+    username: "psmith",
+    email: "pat.smith@somewhere.net",
+    created_at: "2016-02-05",  
+  },
+  {
+    first_name: "Patrick",
+    last_name: "Jones",
+    username: "pjpj",
+    email: "jones.p@business.net",
+    created_at: "2014-03-05",  
+  },
+  {
+    first_name: "Patricia",
+    last_name: "Benjamin",
+    username: "pattyb",
+    email: "benjie@aol.info",
+    created_at: "2016-01-02",  
+  },
+  {
+    first_name: "Patty",
+    last_name: "Patrickson",
+    username: "ppat",
+    email: "pppp@freemail.computer",
+    created_at: "2016-02-05",  
+  },
+  {
+    first_name: "Jane",
+    last_name: "Patrick",
+    username: "janesays",
+    email: "janep@company.net",
+    created_at: "2013-01-05",  
+  },
+];
+
 @Component({
   selector: 'customer-search',
   template: '\
@@ -14,10 +52,10 @@ import { FormsModule } from "@angular/forms";
               <label for="keywords" class="sr-only">Keywords></label> \
               <input type="text" id="keywords" name="keywords" \
                      placeholder="First Name, Last Name, or Email Address"\
-                     class="form-control input">\
+                     class="form-control input" bindon-ngModel="keywords">\
               <span class="input-group-btn"> \
                 <input type="submit" value="Find Customers"\
-                       class="btn btn-primary">\
+                       class="btn btn-primary" on-click="search()">\
               </span> \
             </div> \
           </form> \
@@ -27,18 +65,31 @@ import { FormsModule } from "@angular/forms";
             <h1 class="h3">Results</h1> \
           </header> \
           <ol class="list-group"> \
-            <li class="list-group-item clearfix"> \
+            <li *ngFor="let customer of customers" \
+              class="list-group-item clearfix"> \
               <h3 class="float-right"> \
                 <small class="text-uppercase">Joined</small> \
-                2016-01-01\
+                {{customer.created_at}} \
               </h3> \
               <h2 class="h3"> \
-                Pat Smith\
-                <small>psmith34</small> \
+                {{customer.first_name}} {{customer.last_name}} \
+                <small>{{customer.username}}</small> \
               </h2> \
-              <h4>pat.smith@example.com</h4> \
+              <h4>{{customer.email}}</h4> \
             </li> \
           </ol> \
         </section>'
 })
-export class CustomerSearchComponent {}
+export class CustomerSearchComponent {
+  keywords = "";
+  customers = [];
+
+  search() {
+    if (this.keywords == "pat") {
+      this.customers = RESULTS;
+    }
+    else {
+      this.customers = [];
+    }
+  };
+}
